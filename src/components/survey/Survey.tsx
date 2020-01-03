b9a696d9410e23c3ee6bf69f3a2c1f365699c31d
Neil Zammit <nz3001@gmail.com>
2020-01-03 11:17:39 +0100

    All cases implemented

--- src/components/survey/Survey.tsx
+++ src/components/survey/Survey.tsx
@@ -7,10 +7,11 @@ import Navbar from '../layout/Navbar';
 // Store both state and questions in separate components
 
 const Survey = () => {
-  // Declare new state var
+  // State
+
+  // Question Count
   const [question, setQuestion] = useState(1);
 
-  // Have all state here, pass state methods as props
   // Q1
   const [age, setAge] = useState(0);
   const [gender, setGender] = useState('male');
@@ -18,27 +19,45 @@ const Survey = () => {
   // Q2
   const [ownsSmartphone, setOwnsSmartphone] = useState('yes');
 
+  // Q3
+  const [brand, setBrand] = useState('samsung');
+
+  // Q4
+  const [usedApple, setUsedApple] = useState('yes');
+
+  // Q5
+  const [preferredOS, setPreferredOS] = useState('ios');
+
+  // Q6
+  const [smartphoneUsage, setSmartphoneUsage] = useState('calls');
+
+  // Q7
+  const [buyingFactor, setBuyingFactor] = useState('design');
+
+  // Q8
+  const [smartphoneDescription, setSmartphoneDescription] = useState('flashy');
+
+  // Q9
+  const [nextSmartphone, setNextSmartphone] = useState('samsung');
+
   // Functions
   const nextQuestion = (): void => {
     setQuestion(question + 1);
   };
 
-  // const skipQuestion = (): void => {
-  // call this for when you want to skip question
-  // }
+  const skipQuestion = (): void => {
+    setQuestion(question + 2);
+  };
+
+  const skipTwoQuestions = (): void => {
+    setQuestion(question + 3);
+  };
 
   const endSurvey = (): void => {
     setQuestion(10);
   };
 
-  // <form>
-  //   <input
-  //     type="text"
-  //     value={title}
-  //     onChange={e => setTitle(e.target.value)}
-  //   ></input>
-  // </form>;
-
+  // Cases / Questions
   switch (question) {
     case 1:
       return (
@@ -80,9 +99,6 @@ const Survey = () => {
         </div>
       );
     case 2:
-      // State logs
-      console.log(age);
-      console.log(gender);
       return (
         <div>
           <Navbar />
@@ -111,18 +127,29 @@ const Survey = () => {
         </div>
       );
     case 3:
-      // State logs
-      console.log(age);
-      console.log(gender);
-      console.log(ownsSmartphone);
       return (
         <div>
           <Navbar />
           <div className="container">
-            <h1>Question {question}</h1>
+            <h1>Which smartphone brand is your main phone?</h1>
+            <form onSubmit={e => e.preventDefault()}>
+              <select
+                className="text-margin"
+                value={brand}
+                onChange={e => setBrand(e.target.value)}
+              >
+                <option value="samsung">Samsung</option>
+                <option value="apple">Apple</option>
+                <option value="oneplus">Oneplus</option>
+                <option value="xiaomi">Xiaomi</option>
+                <option value="other">Other</option>
+              </select>
+            </form>
             <button
               className="button"
-              onClick={() => setQuestion(question + 1)}
+              onClick={() =>
+                brand !== 'apple' ? nextQuestion() : skipTwoQuestions()
+              }
               style={{ padding: '0.7rem 3rem', marginTop: '2rem' }}
             >
               Next
@@ -135,10 +162,22 @@ const Survey = () => {
         <div>
           <Navbar />
           <div className="container">
-            <h1>Question {question}</h1>
+            <h1>Have you ever used an iPhone?</h1>
+            <form onSubmit={e => e.preventDefault()}>
+              <select
+                className="text-margin"
+                value={usedApple}
+                onChange={e => setUsedApple(e.target.value)}
+              >
+                <option value="yes">Yes</option>
+                <option value="no">No</option>
+              </select>
+            </form>
             <button
               className="button"
-              onClick={() => setQuestion(question + 1)}
+              onClick={() =>
+                usedApple === 'yes' ? nextQuestion() : skipQuestion()
+              }
               style={{ padding: '0.7rem 3rem', marginTop: '2rem' }}
             >
               Next
@@ -146,15 +185,26 @@ const Survey = () => {
           </div>
         </div>
       );
+
     case 5:
       return (
         <div>
           <Navbar />
           <div className="container">
-            <h1>Question {question}</h1>
+            <h1>Do you prefer iOS (iPhone Operating System) or Android?</h1>
+            <form onSubmit={e => e.preventDefault()}>
+              <select
+                className="text-margin"
+                value={preferredOS}
+                onChange={e => setPreferredOS(e.target.value)}
+              >
+                <option value="ios">iOS</option>
+                <option value="android">Android</option>
+              </select>
+            </form>
             <button
               className="button"
-              onClick={() => setQuestion(question + 1)}
+              onClick={() => nextQuestion()}
               style={{ padding: '0.7rem 3rem', marginTop: '2rem' }}
             >
               Next
@@ -167,10 +217,27 @@ const Survey = () => {
         <div>
           <Navbar />
           <div className="container">
-            <h1>Question {question}</h1>
+            <h1>What do you mostly use your phone for?</h1>
+            <form onSubmit={e => e.preventDefault()}>
+              <select
+                className="text-margin"
+                value={smartphoneUsage}
+                onChange={e => setSmartphoneUsage(e.target.value)}
+              >
+                <option value="calls">Making phone calls</option>
+                <option value="messaging">
+                  Sending messages (text, email or online messaging)
+                </option>
+                <option value="social media">
+                  Browsing social media (Facebook, Instagram, Twitter)
+                </option>
+                <option value="photos">Taking Photos and Video</option>
+                <option value="games">Playing Games</option>
+              </select>
+            </form>
             <button
               className="button"
-              onClick={() => setQuestion(question + 1)}
+              onClick={() => nextQuestion()}
               style={{ padding: '0.7rem 3rem', marginTop: '2rem' }}
             >
               Next
@@ -183,10 +250,23 @@ const Survey = () => {
         <div>
           <Navbar />
           <div className="container">
-            <h1>Question {question}</h1>
+            <h1>What is the most important factor when buying a smartphone?</h1>
+            <form onSubmit={e => e.preventDefault()}>
+              <select
+                className="text-margin"
+                value={buyingFactor}
+                onChange={e => setBuyingFactor(e.target.value)}
+              >
+                <option value="design">Design</option>
+                <option value="performance">Performance</option>
+                <option value="price">Value for money</option>
+                <option value="camera">Good Quality Camera</option>
+                <option value="other">Other</option>
+              </select>
+            </form>
             <button
               className="button"
-              onClick={() => setQuestion(question + 1)}
+              onClick={() => nextQuestion()}
               style={{ padding: '0.7rem 3rem', marginTop: '2rem' }}
             >
               Next
@@ -199,10 +279,21 @@ const Survey = () => {
         <div>
           <Navbar />
           <div className="container">
-            <h1>Question {question}</h1>
+            <h1>Which of the following describes your phone best?</h1>
+            <form onSubmit={e => e.preventDefault()}>
+              <select
+                className="text-margin"
+                value={smartphoneDescription}
+                onChange={e => setSmartphoneDescription(e.target.value)}
+              >
+                <option value="flashy">Flashy</option>
+                <option value="durable">Durable</option>
+                <option value="lightweight">Lightweight</option>
+              </select>
+            </form>
             <button
               className="button"
-              onClick={() => setQuestion(question + 1)}
+              onClick={() => nextQuestion()}
               style={{ padding: '0.7rem 3rem', marginTop: '2rem' }}
             >
               Next
@@ -215,10 +306,25 @@ const Survey = () => {
         <div>
           <Navbar />
           <div className="container">
-            <h1>Question {question}</h1>
+            <h1>
+              If you were to switch your phone, what brand would you buy next?
+            </h1>
+            <form onSubmit={e => e.preventDefault()}>
+              <select
+                className="text-margin"
+                value={nextSmartphone}
+                onChange={e => setNextSmartphone(e.target.value)}
+              >
+                <option value="samsung">Samsung</option>
+                <option value="apple">Apple</option>
+                <option value="oneplus">Oneplus</option>
+                <option value="xiaomi">Xiaomi</option>
+                <option value="other">Other</option>
+              </select>
+            </form>
             <button
               className="button"
-              onClick={() => setQuestion(question + 1)}
+              onClick={() => nextQuestion()}
               style={{ padding: '0.7rem 3rem', marginTop: '2rem' }}
             >
               Next
@@ -227,6 +333,17 @@ const Survey = () => {
         </div>
       );
     case 10:
+      // State logs
+      console.log(`Age: ${age}`);
+      console.log(`Gender: ${gender}`);
+      console.log(`Smartphone Owner: ${ownsSmartphone}`);
+      console.log(`Smartphone Brand: ${brand}`);
+      console.log(`Used iPhone: ${usedApple}`);
+      console.log(`Preferred OS: ${preferredOS}`);
+      console.log(`Main Use: ${smartphoneUsage}`);
+      console.log(`Main Buying Factor: ${buyingFactor}`);
+      console.log(`Smartphone Description: ${smartphoneDescription}`);
+      console.log(`Next Smartphone Brand: ${nextSmartphone}`);
       return (
         <div>
           <Navbar />
@@ -238,21 +355,7 @@ const Survey = () => {
       );
   }
 
-  return (
-    <div>
-      <Navbar />
-      <div className="container">
-        <h1>Question {question}</h1>
-        <button
-          className="button"
-          onClick={() => setQuestion(question + 1)}
-          style={{ padding: '0.7rem 3rem', marginTop: '2rem' }}
-        >
-          Next
-        </button>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Survey;
